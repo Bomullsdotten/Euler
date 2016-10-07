4c249659baa0f1a6af6795839ef6bb4215c91a99
Bomullsdotten <sondre_erstad@hotmail.com>
2016-10-07 15:20:11 +0200

    refactoring

--- primes/Eratosthenes.py
+++ primes/Eratosthenes.py
@@ -1,20 +1,15 @@
 import math
+
+
 def primesTo(upTo):
+    knownPrimes = [2]
     # to offset lists starting at 0
     upTo += 1
+    square_of_upTo = math.sqrt(upTo)
+    unknownPrimes = [number for number in xrange(3, upTo, 2)]
+    for position, posiblePrime in enumerate(unknownPrimes):
+        if posiblePrime and posiblePrime <= square_of_upTo:
+            for nonPrime in xrange(position+posiblePrime, len(unknownPrimes), posiblePrime):
+                unknownPrimes[nonPrime] = None
 
-    primes = [True]*upTo
-    primes[0] = False
-    primes[1] = False
-    for number in xrange(2, int(math.sqrt(upTo))+1):
-        if primes[number]:
-            for nonPrime in xrange(number*2, upTo, number):
-                primes[nonPrime] = False
-
-    return [position for position, value in enumerate(primes) if value]
-
-
-
-
-if __name__ == '__main__':
-    pass
+    return knownPrimes + [prime for prime in unknownPrimes if prime]
